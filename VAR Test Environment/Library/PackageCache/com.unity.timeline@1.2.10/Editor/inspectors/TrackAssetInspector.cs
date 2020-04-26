d2661fe10130dccfd933ddb3cae4ac22d8135460
mkr64294 <mark-rice@bethel.edu>
2020-04-26 14:17:51 -0500

    Added back OnDefense script
    Added back OnDefense script

--- VAR Test Environment/Library/PackageCache/com.unity.timeline@1.2.10/Editor/inspectors/TrackAssetInspector.cs
+++ VAR Test Environment/Library/PackageCache/com.unity.timeline@1.2.10/Editor/inspectors/TrackAssetInspector.cs
@@ -118,6 +118,18 @@ namespace UnityEditor.Timeline
             }
         }
 
+        /*internal override Rect DrawHeaderHelpAndSettingsGUI(Rect r)
+        {
+            using (new EditorGUI.DisabledScope(IsTrackLocked()))
+            {
+                var helpSize = EditorStyles.iconButton.CalcSize(EditorGUI.GUIContents.helpIcon);
+                const int kTopMargin = 5;
+
+                // Show Editor Header Items.
+                return EditorGUIUtility.DrawEditorHeaderItems(new Rect(r.xMax - helpSize.x, r.y + kTopMargin, helpSize.x, helpSize.y), targets);
+            }
+        }*/
+
         public virtual void OnEnable()
         {
             m_IsBuiltInType = target != null && target.GetType().Assembly == typeof(TrackAsset).Assembly;
