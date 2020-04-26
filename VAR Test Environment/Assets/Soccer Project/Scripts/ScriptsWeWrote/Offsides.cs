b34a486670d6c54f02dbd840ebe1ab1a8eae3a2e
alexwisuri <alexwisuri@gmail.com>
2020-04-26 13:35:27 -0500

    Revised and Refactored Offsides Scripts

--- VAR Test Environment/Assets/Soccer Project/Scripts/ScriptsWeWrote/Offsides.cs
+++ VAR Test Environment/Assets/Soccer Project/Scripts/ScriptsWeWrote/Offsides.cs
@@ -4,102 +4,174 @@ using UnityEngine;
 
 public class Offsides : MonoBehaviour
 {
-    public GameObject[] BluePlayers;
-    public GameObject[] RedPlayers;
-    //float[] BluePlayerPositions;
-    //float[] RedPlayerPositions;
-    public GameObject RedDotBlue;
-    public GameObject GreenDotBlue;
-    public GameObject RedDotRed;
-    public GameObject GreenDotRed;
-    public List<GameObject> OffBlue;
-    public List<GameObject> OffRed;
-    float BlueMax;
-    float BlueMin;
-    float RedMax;
-    float RedMin;
-    float BlueGK;
-    float RedGK;
-    
-//    void OutlineTest() {
-//        // Outline stuff
-//        var outline = BluePlayers[7].AddComponent<Outline>();
-//
-//        outline.OutlineMode = Outline.Mode.OutlineAll;
-//        outline.OutlineColor = Color.red;
-//        outline.OutlineWidth = 5f;
-//        
-//        outline.enable();
-//    }
+    public GameObject ball;
+    public GameObject BlueGoalKeeper;
+    public GameObject RedGoalKeeper;
+    public BlueOffsides BlueOnAttack;
+    public RedOffsides RedOnAttack;
+    public OnDefense SwitchHalves;
 
+    void Start()
+    {
+        BlueOnAttack = RedGoalKeeper.GetComponent<BlueOffsides>();
+        RedOnAttack = BlueGoalKeeper.GetComponent<RedOffsides>();
+        SwitchHalves = ball.GetComponent<OnDefense>();
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        BlueMax = 0;
-        BlueMin = 0;
-        RedMax = 0;
-        RedMin = 0;
 
-        for (int i = 0; i < 10; i++)
-        {
-            //BluePlayerPositions[i] = BluePlayers[i].transform.position.z;
-           // RedPlayerPositions[i] = RedPlayers[i].transform.position.z;
+        if (ball.transform.position.z > 0 && !BlueOnAttack.enabled)
+            BlueAttack();
+        else if (ball.transform.position.z < 0 && !RedOnAttack.enabled)
+            RedAttack();
+        else
+            Midline();
+    }
 
-            if (BluePlayers[i].transform.position.z > BlueMax)
-            {
-                BlueMax = BluePlayers[i].transform.position.z;
-                OffBlue.Add(BluePlayers[i]);
-            }
-            if (BluePlayers[i].transform.position.z < BlueMin)
-            {
-                BlueMin = BluePlayers[i].transform.position.z;
-                OffBlue.Clear();
-            }
-            if (RedPlayers[i].transform.position.z > RedMax)
-            {
-                RedMax = RedPlayers[i].transform.position.z;
-                OffRed.Add(RedPlayers[i]);
-            }
-            if (RedPlayers[i].transform.position.z < RedMin)
-            {
-                RedMin = RedPlayers[i].transform.position.z;
-                OffRed.Clear();
-            }
-        }
+    public void RedAttack()
+    {
+        RedOnAttack.enabled = true;
+    }
 
+    public void BlueAttack()
+    {
+        BlueOnAttack.enabled = true;
+    }
+
+    public void Midline()
+    {
+        RedOnAttack.enabled = false;
+        BlueOnAttack.enabled = false;
+        SwitchHalves.enabled = true;
+        SwitchHalves.enabled = false;
+    }
+}
+//    public GameObject[] BluePlayers;
+//    public GameObject[] RedPlayers;
+//    public GameObject RedDotBlue;
+//   public GameObject GreenDotBlue;
+//    public GameObject RedDotRed;
+//    public GameObject GreenDotRed;
+//    public List<GameObject> OffBlue = new List<GameObject>();
+//    public List<GameObject> OffRed = new List<GameObject>();
+//    float BlueMax;
+//    float BlueMin;
+//    float RedMax;
+//    float RedMin;
+
+    // Update is called once per frame
+//    void Update()
+//    {
+        //BlueMin = 0;
+        //RedMax = 0;
+
+        ////Get second to last player of each team
+        //setMAXMIN(BluePlayers, ref BlueMin);
+        //setMAXMIN(RedPlayers, ref RedMax);
+
+        ////Get offsides blue and red players
+        //getOffsidesPlayers(BluePlayers, OffBlue, RedMax);
+        //getOffsidesPlayers(RedPlayers, OffRed, BlueMin);
+
+        ////Check if offsides players are now onsides
+        //checkOffsidesPlayers(OffBlue, RedMax); 
+        //checkOffsidesPlayers(OffRed, BlueMin);          
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    if (BluePlayers[i].transform.position.z > BlueMax)
+        //    {
+        //        BlueMax = BluePlayers[i].transform.position.z;
+        //        OffBlue.Add(BluePlayers[i]);
+        //    }
+        //    if (BluePlayers[i].transform.position.z < BlueMin)
+        //    {
+        //        BlueMin = BluePlayers[i].transform.position.z;
+        //        OffBlue.Clear();
+        //    }
+        //    if (RedPlayers[i].transform.position.z > RedMax)
+        //    {
+        //        RedMax = RedPlayers[i].transform.position.z;
+        //        OffRed.Add(RedPlayers[i]);
+        //    }
+        //    if (RedPlayers[i].transform.position.z < RedMin)
+        //    {
+        //        RedMin = RedPlayers[i].transform.position.z;
+        //        OffRed.Add(RedPlayers[i]);
+        //    }
+        //}
 
         //BlueGK = BluePlayerPositions[10];
         //RedGK = RedPlayerPositions[10];
 
-        // Blue player offsides
-        if (BlueMax > RedMax && GreenDotBlue.activeSelf == true)
-        {
-            foreach (GameObject player in OffBlue)
-                player.GetComponentInChildren<Outline>().enabled = true;
-            RedDotBlue.SetActive(true);
-            GreenDotBlue.SetActive(false);
-        }
-        if (BlueMax < RedMax && GreenDotBlue.activeSelf == false)
-        {
-            foreach (GameObject player in OffBlue)
-                player.GetComponentInChildren<Outline>().enabled = false;
-            RedDotBlue.SetActive(false);
-            GreenDotBlue.SetActive(true);
-        }
-        if (BlueMin > RedMin && GreenDotRed.activeSelf == true)
-        {
-            foreach (GameObject player in OffRed)
-                player.GetComponentInChildren<Outline>().enabled = true;
-            RedDotRed.SetActive(true);
-            GreenDotRed.SetActive(false);
-        }
-        if (BlueMin < RedMin && GreenDotRed.activeSelf == false)
-        {
-            foreach (GameObject player in OffRed)
-                player.GetComponentInChildren<Outline>().enabled = false;
-            RedDotRed.SetActive(false);
-            GreenDotRed.SetActive(true);
-        }
-    }
-}
+        //// Blue player offsides
+        //if (BlueMax > RedMax && GreenDotBlue.activeSelf == true)
+        //{
+        //    foreach (GameObject player in OffBlue)
+        //        player.GetComponentInChildren<Outline>().enabled = true;
+        //    RedDotBlue.SetActive(true);
+        //    GreenDotBlue.SetActive(false);
+        //}
+        //if (BlueMax < RedMax && GreenDotBlue.activeSelf == false)
+        //{
+        //    foreach (GameObject player in OffBlue)
+        //        player.GetComponentInChildren<Outline>().enabled = false;
+        //    RedDotBlue.SetActive(false);
+        //    GreenDotBlue.SetActive(true);
+        //}
+        //if (BlueMin > RedMin && GreenDotRed.activeSelf == true)
+        //{
+        //    foreach (GameObject player in OffRed)
+        //        player.GetComponentInChildren<Outline>().enabled = true;
+        //    RedDotRed.SetActive(true);
+        //    GreenDotRed.SetActive(false);
+        //}
+        //if (BlueMin < RedMin && GreenDotRed.activeSelf == false)
+        //{
+        //    foreach (GameObject player in OffRed)
+        //        player.GetComponentInChildren<Outline>().enabled = false;
+        //    RedDotRed.SetActive(false);
+        //    GreenDotRed.SetActive(true);
+        //}
+//    }
+
+    //private void setMAXMIN(GameObject[] Players, ref float BRMaxMin)
+    //{
+    //    for(int i = 0; i < Players.Length - 1; ++i)
+    //    {
+    //        if (System.Math.Abs(Players[i].transform.position.z) > System.Math.Abs(BRMaxMin))
+    //            BRMaxMin = System.Math.Abs(Players[i].transform.position.z);
+    //    }
+    //}
+
+    //private void getOffsidesPlayers(GameObject[] Players, List<GameObject> Offsides, float reference)
+    //{ 
+    //    for(int i = 0; i < Players.Length - 1; ++i)
+    //    {
+    //        if (System.Math.Abs(Players[i].transform.position.z) > reference && !Offsides.Contains(Players[i]))
+    //        {
+    //            Players[i].GetComponentInChildren<Outline>().enabled = true;
+    //            //EnableDisableOffsides(Players[i], true);
+    //            Offsides.Add(Players[i]);
+    //        }
+    //    }
+    //}
+
+    //private void checkOffsidesPlayers(List<GameObject> Offsides, float reference)
+    //{
+    //    for (int i = 0; i < Offsides.Count; ++i)
+    //    {
+    //        if (System.Math.Abs(Offsides[i].transform.position.z) < reference)
+    //        {
+    //            Offsides[i].GetComponentInChildren<Outline>().enabled = false;
+    //            //EnableDisableOffsides( Offsides[i], false);
+    //            Offsides.Remove(Offsides[i]);
+    //        }
+    //    }
+    //}
+
+    //private void EnableDisableOffsides(GameObject player, bool offsides)
+    //{
+    //    player.GetComponentInChildren<Outline>().enabled = offsides;
+    //}
---
+++ VAR Test Environment/Assets/Soccer Project/Scripts/ScriptsWeWrote/OnDefense.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDefense : MonoBehaviour
+{
+    public GameObject[] Players;
+    public List<Outline> PlayersOutline = new List<Outline>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Players.Length; ++i)
+            PlayersOutline.Add(Players[i].GetComponentInChildren<Outline>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < PlayersOutline.Count; ++i)
+            PlayersOutline[i].enabled = false;
+    }
+}
